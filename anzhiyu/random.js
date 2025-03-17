37f8600663cda6f4309cb31fdd39eb6ef31f996a
Hu-list <1293549262@qq.com>
2025-03-17 18:20:40 +0800

    Site updated: 2025-03-17 18:20:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/17/hello-world/"];function toRandomPost(){
+var posts=["2025/03/17/这是一篇HTML/","2025/03/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
